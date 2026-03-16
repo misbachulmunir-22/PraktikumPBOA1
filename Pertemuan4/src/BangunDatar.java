444e4999b363c2d2ec0d5d68166cadf26ac9680f
misbachulmunir-22 <almunirmisbah22@gmail.com>
2026-03-16 09:32:04 +0700

    Add files via upload

---
+++ Pertemuan4/src/BangunDatar.java
@@ -0,0 +1,45 @@
+public class BangunDatar {
+    protected int jmlSisi;
+    protected String warna;
+    private String border;
+
+    public BangunDatar() {
+    }
+
+    public BangunDatar(int jmlSisi, String warna, String border) {
+        this.jmlSisi = jmlSisi;
+        this.warna = warna;
+        this.border = border;
+    }
+
+    public int getJmlSisi() {
+        return jmlSisi;
+    }
+
+    public void setJmlSisi(int jmlSisi) {
+        this.jmlSisi = jmlSisi;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getBorder() {
+        return border;
+    }
+
+    public void setBorder(String border) {
+        this.border = border;
+    }
+
+    public void printInfo() {
+        System.out.println("Jumlah sisi: " + jmlSisi);
+        System.out.println("Warna: " + warna);
+        System.out.println("Border: " + border);
+
+    }
+}
